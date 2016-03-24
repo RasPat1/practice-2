72532312ff4923bad04ac1530511a38d2fa1e7c1
Rasesh Patel <Rasesh@shoptiques.com>
2016-03-24 10:19:34 -0400

    Update hash table Impl

--- HashTable.java
+++ HashTable.java
@@ -10,23 +10,23 @@
   *
   **/
  class HashTable {
+ 	Integer[] table;
 
  	public static void main(String[] args) {
  		// Test Runner
-
- 		Integer dictionarySize = 100;
-
  		Integer k1 = 10;
  		Integer k2 = 9;
  		Integer k3 = 12;
+
 
  		HashTable t1 = new HashTable();
  		t1.insert(k1);
  		t1.insert(k2);
  		Integer result1 = t1.search(k1);
- 		assert result1 == k1
+ 		assert result1 == k1;
  		Integer result2 = t1.search(k2);
- 		assert result2 == k2
+ 		assert result2 == k2;
+
 
  		t1.delete(k1);
  		Integer result3 = t1.search(k1);
@@ -36,17 +36,54 @@
  		Integer result4 = t1.search(k2);
  		assert result4 == null;
 
+
+		System.out.println(t1);
+ 		System.out.println("Succesfully run");
+
  	}
 
  	Boolean insert(Integer key) {
-
+ 		return false;
  	}
 
  	Integer search(Integer key) {
-
+ 		return 0;
  	}
 
+ 	Boolean delete(Integer key) {
+ 		return false;
+ 	}
 
+ 	/**
+ 	 * Intialize Hash Table
+ 	 */
+ 	public void HashTable(Integer dictionarySize) {
+ 		this.table = new Integer[dictionarySize];
+ 	}
+
+ 	/**
+ 	 * Return a Hash of the integer using a safe Hash function
+ 	 **/
+ 	Integer hash(Integer key) {
+ 		return 0;
+ 	}
+
+ 	public String toString() {
+ 		String string = "START::";
+
+ 		for (int i = 0; i < this.table.length; i++) {
+ 			String value = "NULL::";
+ 			if (this.table[i] != null) {
+ 				value = this.table[i] + "::";
+ 			}
+
+ 			string += value;
+ 		}
+
+ 		string += "END";
+
+ 		return string;
+ 	}
 
  }
 
